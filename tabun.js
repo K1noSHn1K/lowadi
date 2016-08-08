1154e0ece2c77ec0490e0f12decff9e204d71461
Crasher69 <kinoshnik69@gmail.com>
2016-08-08 15:53:43 +0300

    Update tabun.js

--- tabun.js
+++ tabun.js
@@ -778,7 +778,7 @@ function settings()
 	{
 		$('body#global').append('<div class="lwb_logo" style="display: block; position: fixed; width: 105px; top: 30px; left: 20px; z-index: 900;"><img src="https://raw.githubusercontent.com/Crasher69/lowadi/master/robothorseday.png" width="100px"></div>');
 		$('body#global').append('<div class="lwb" style="display:block; position:fixed; width:120px; height:115px; left:0; top:105px; padding:5px; background-color:rgba(0, 0, 0, 0.7);  border-radius: 0px 0px 20px 0;"></div>');
-		$('.lwb').append('<span class="header-currency-label" style="color:#fafe6c;  z-index:990;"><b>LwBot v1.4</b></span>  <span class="lwb_chat_button" style="cursor:pointer; position:absolute; right:30px; top:3px;>!</span>  <span class="lwb_setting" style="cursor:pointer; position:absolute; right:5px; top:3px; z-index:999;">  <img src="https://raw.githubusercontent.com/Crasher69/lowadi/master/settings-n.png" width="20px" title="Показать настройки" /></span>');
+		$('.lwb').append('<span class="header-currency-label" style="color:#fafe6c;  z-index:990;"><b>LwBot v1.4</b></span>  <span class="lwb_chat_button" style="cursor:pointer;  top:3px;>!</span>  <span class="lwb_setting" style="cursor:pointer; position:absolute; right:5px; top:3px; z-index:999;">  <img src="https://raw.githubusercontent.com/Crasher69/lowadi/master/settings-n.png" width="20px" title="Показать настройки" /></span>');
 		
 		if (is_lic()===true)
 		{
@@ -805,7 +805,7 @@ function settings()
 		
 		$('body#global').append('<div class="lwb_chat" style="display: none; position: fixed; width: 570px; height:630px; top: 105px; left: 130px; z-index: 999; padding:5px; background-color:rgba(0, 0, 0, 0.85);  border-radius: 0px 5px 5px 5px;"></div>');
 		$('.lwb_chat').append('<center><h2 style="color:#fff;">Обсуждение</h2> </center> <span class="lwb_chat_button" style="position:absolute;  right:5px; top:2px; color:#fff; cursor:pointer;"><b>X</b></span>');
-		$('.lwb_chat').append('<div id="disqus_thread"></div> <script>  var disqus_config = function () {     this.page.url = PAGE_URL;       this.page.identifier = PAGE_IDENTIFIER;  };  (function() {     var d = document, s = d.createElement('script');     s.src = '//lwbot.disqus.com/embed.js';     s.setAttribute('data-timestamp', +new Date());     (d.head || d.body).appendChild(s); })(); </script> ');	
+		$('.lwb_chat').append('<div id="disqus_thread"></div> <script>  var disqus_config = function () {     this.page.url = PAGE_URL;       this.page.identifier = PAGE_IDENTIFIER;  };  (function() {     var d = document, s = d.createElement(\'script\');     s.src = \'//lwbot.disqus.com/embed.js\';     s.setAttribute(\'data-timestamp\', +new Date());     (d.head || d.body).appendChild(s); })(); </script> ');	
 		$('.lwb_settings').append('<br><center><button id="lwb_savesettings" style="margin: 5px 0 0 0;" onclick="savesettings();" class="button button-style-0"><span class="button-align-0"><span class="button-inner-0"><span class="button-text-0">Сохранить</span></span></span></button></center>');
 
 
