9697e0bd65668a55c288be0d5092b421221865e3
Crasher69 <kinoshnik69@gmail.com>
2016-08-09 14:38:33 +0300

    Update tabun.js

--- tabun.js
+++ tabun.js
@@ -1039,12 +1039,12 @@ function check_shablon()
 
  		
 /*Disqus*/
-/*
+
 var disqus_config = function () {
-    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    this.page.url = "/";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = "lwbot"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
-*/
+
 (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
     s.src = '//lwbot.disqus.com/embed.js';
