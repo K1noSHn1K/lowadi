ecd36fe0c73d0317bad696b30aaf8917dbce69aa
Crasher69 <kinoshnik69@gmail.com>
2016-08-09 16:50:26 +0300

    Update tabun.js

--- tabun.js
+++ tabun.js
@@ -1041,7 +1041,7 @@ function check_shablon()
 /*Disqus*/
 
 var disqus_config = function () {
-  //  this.page.url = "/";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.url = "http://lowadi.com";  // Replace PAGE_URL with your page's canonical URL variable
     this.page.identifier = "lwbot"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
 
