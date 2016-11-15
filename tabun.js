d30da0b248a04bcc50d88a7c4672f3386383b6d0
Crasher69 <kinoshnik69@gmail.com>
2016-11-15 14:14:34 +0300

    Update tabun.js

--- tabun.js
+++ tabun.js
@@ -3,7 +3,7 @@
 // @description Бот для браузерной игры Lowadi
 // @author HumanoID
 // @license Mozilla Public License Version 2.0
-// @version 1.4
+// @version 1.4.3
 // @include http://www.lowadi.com/*
 // @grant none
 // ==/UserScript==
