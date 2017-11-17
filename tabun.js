4eaa5fc283cf2f0fe5c5e97ffdbb9b322240ef07
Crasher69 <kinoshnik69@gmail.com>
2017-11-17 13:46:53 +0300

    Update tabun.js

--- tabun.js
+++ tabun.js
@@ -369,8 +369,8 @@ function usualProg()
 	setTimeout(stroke, pause5);
 	  // Спать
 	  var pause6 = pause5 + getRandomPause(600, 900+SPEED);
-	//setTimeout(sleep, pause6);
-	setTimeout(function() { $('#form-do-night').submit(); }, pause6);		
+	setTimeout(sleep, pause6);
+	//setTimeout(function() { $('#form-do-night').submit(); }, pause6);		
 	    var pause7 = pause6 + getRandomPause(600, 900+SPEED);
 	setTimeout(stroke, pause7);
 	setTimeout(minEnergy,pause7+400);
